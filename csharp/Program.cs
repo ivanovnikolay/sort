5f14b6423b2196b62d10f4fe6351457e692ef98d
Nikolay Ivanov <nikolay.ivanov@openmedia.co>
2023-02-05 17:40:06 +0100

    Add csharp implementation

---
+++ csharp/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.WriteLine("Bubble sort:");
+int[] array = {0, 4, 3, 5, 2, 1};
+Console.WriteLine("[{0}]", string.Join(", ", BubbleSort(array)));
+
+
+int [] BubbleSort(int[] a) {
+    var N = a.Length;
+    var swapped = true;
+    while (swapped) {
+        N--;
+        swapped = false;
+        for (var i = 0; i < N; i++) {
+            if (a[i] > a[i + 1]) {
+                var temp = a[i];
+                a[i] = a[i + 1];
+                a[i + 1] = temp;
+                swapped = true;
+            }
+        }
+    }
+    return a;
+}
---
+++ csharp/readme.md
@@ -0,0 +1,24 @@
+# Install (MacOS)
+
+Open Extensions `Ctrl+X` in Visual Code then type `C#` in the search field and install C# plugin `C# for Visual Studio Code (powered by OmniSharp)` by Microsoft.
+
+Install `dotnet`:
+```bash
+brew install dotnet
+```
+
+Create the project:
+```bash
+dotnet new console -o csharp
+cd csharp
+```
+
+Build the project:
+```bash
+dotnet build
+```
+
+Run the project:
+```bash
+dotnet run
+```
